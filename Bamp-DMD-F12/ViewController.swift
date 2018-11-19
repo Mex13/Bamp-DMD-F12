83d7aed28704f7963074c974eaca0d4bf3e73f4a
Lucia Mizicova (s5105866) <s5105866@mac42945.student.bournemouth.ac.uk>
2018-11-19 14:14:00 +0000

    Initial Commit

---
+++ Bamp-DMD-F12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bamp-DMD-F12
+//
+//  Created by Lucia Mizicova (s5105866) on 19/11/2018.
+//  Copyright © 2018 Lucia Mizicova (s5105866). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bamp-DMD-F12Tests/Bamp_DMD_F12Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Bamp_DMD_F12Tests.swift
+//  Bamp-DMD-F12Tests
+//
+//  Created by Lucia Mizicova (s5105866) on 19/11/2018.
+//  Copyright © 2018 Lucia Mizicova (s5105866). All rights reserved.
+//
+
+import XCTest
+@testable import Bamp_DMD_F12
+
+class Bamp_DMD_F12Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
