96a9dce91b1a065fc595d0a85c844073bcba32f5
Lucia Mizicova (s5105866) <s5105866@mac42945.student.bournemouth.ac.uk>
2018-11-19 14:39:00 +0000

    Removed comments

--- Bamp-DMD-F12/ViewController.swift
+++ Bamp-DMD-F12/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Bamp-DMD-F12
-//
-//  Created by Lucia Mizicova (s5105866) on 19/11/2018.
-//  Copyright © 2018 Lucia Mizicova (s5105866). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
