2a0498e79b903bf8a979af82116d913e1df3dfd7
Lucia Mizicova (s5105866) <s5105866@mac38574.student.bournemouth.ac.uk>
2018-11-22 13:32:45 +0000

    SportPlaces added and ViewController

--- Bamp-DMD-F12/SportPlaces.swift
+++ Bamp-DMD-F12/SportPlaces.swift
@@ -1,9 +1,38 @@
-//
-//  SportPlaces.swift
-//  Bamp-DMD-F12
-//
-//  Created by Lucia Mizicova (s5105866) on 20/11/2018.
-//  Copyright © 2018 Lucia Mizicova (s5105866). All rights reserved.
-//
+import Firebase
+import CoreLocation
 
-import Foundation
+protocol SportPlaceDelegate {
+    func didUpdate()
+}
+
+class SportPlaces {
+    let name: String!
+    
+    var delegate: SportplacesDelegate?
+    var sports = [Sport]()
+    
+    init (name: String) {
+        self.name = name
+    }
+    func startPlaceSport() {
+        let ref = Firestore.firestore().collection("locations")
+        ref.addSnapshotListener { snapshot, error in
+            guard let snapshot = snapshot else { return }
+            self.sports.removeAll()
+            for document in snapshot.documents {
+                if let sport = Sport(snapshot: document) {
+                    self.fsports.append(friend)
+                }
+                self.delegate?.didUpdate()
+            }
+            
+        }
+        
+    }
+    func setLocation(_ location: CLLocation) {
+        let ref = Firestore.firestore().collection("locations").document(name)
+        let geo = GeoPoint(latitude: location.coordinate.latitude, longitude: location.coordinate.latitude)
+        ref.setData(["location": geo])
+    }
+    
+}
--- Bamp-DMD-F12/ViewController.swift
+++ Bamp-DMD-F12/ViewController.swift
@@ -2,18 +2,18 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
- 
+    
     let locationManager = CLLocationManager()
     let sport = SportPlaces(name: "Football")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-    sport.delegate = self
-    sport.startPlacesSports()
-   
+        
+        sport.delegate = self
+        sport.startPlacesSports()
+        
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
@@ -23,32 +23,46 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: CLLocationManagerDelegate {
-    func didUpdate() {
-        if tableView.numberOfRows(inSection: 0) = places.sport.count {
-           tableView.reloadData()
-        }
-        if let indexPaths = tableView.indexPathsForVisibleRows {
-            tableView.reloadRows(at: indexPaths, with: .fade)
-        }
-    }
-    func locationManager( manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    func locationManager( manager: CLLocationManager, didUpdateLocations locations, _: [CLLocation]) {
         guard let location = locations.last else {return}
-        places.setLocation(location)
+        print(location)
     }
-}
-extension ViewController: UITableViewDataSource {
-    func tableView( tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.sport.count
-    }
-    func tableView( tableView: UITableView, cellOfRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell"), for: IndexPath)
-        let SportForCell = places.sport[IndexPath.row]
-        cell?.textLabel?.text = SportForCell.name
-        cell.detailTextLabel?text = " \"(sportForCell.location.latitude) - \
-        (SportForCell.location.longitude)"
-        return cell
+    extension ViewController: UITableViewDataSource, UITableViewDelegate {
+        func tableView( tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return places.sport.count
+        }
+        func tableView( tableView: UITableView, cellOfRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell"), for: IndexPath)
+            let SportForCell = places.sport[IndexPath.row]
+            cell?.textLabel?.text = SportForCell.name
+            cell.detailTextLabel?text = " \"(sportForCell.location.latitude) - \
+            (SportForCell.location.longitude)"
+            return cell
+        }
         
-    }
-}
         
+        func didUpdate() {
+            if tableView.numberOfRows(inSection: 0) = places.sport.count {
+                tableView.reloadData()
+            }
+            if let indexPaths = tableView.indexPathsForVisibleRows {
+                tableView.reloadRows(at: indexPaths, with: .fade)
+            }
+        }
+        
+        func tableView ( tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
+            tableView.reloadRows (at: IndexPath, with: .fade)
+        }
+        extension ViewController: CLLocationManagerDelegate {
+            
+            func locationManager ( manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+                guard let location = locations.last else {return}
+                
+                
+            }
+}
+
+
 
