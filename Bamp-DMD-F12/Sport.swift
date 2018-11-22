72531721eed429fe037f0171e771932d6b6743df
Lucia Mizicova (s5105866) <s5105866@mac38574.student.bournemouth.ac.uk>
2018-11-22 13:44:25 +0000

    Sport

--- Bamp-DMD-F12/Sport.swift
+++ Bamp-DMD-F12/Sport.swift
@@ -1,1 +1,24 @@
-import Foundation
+import Firebase
+import MapKit
+
+class Sport: NSObject, MKAnnotation {
+    
+    @objc dynamic var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+    }
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return status
+    }
+    let location: GeoPoint
+    let name: String?
+    
+    init(snapshot: QueryDocumentSnapshot) {
+        guard let location = snapshot.data()["location"] as? GeoPoint else {return nil}
+        self.location = location
+        name = snapshot.documentID
+        status = snapshot.data()["status"] as? String
+}
