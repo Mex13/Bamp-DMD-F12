e027da6124f370ae2d7a634d70e6348eb889891a
Laura McDonald (s5102942) <s5102942@mac42944.student.bournemouth.ac.uk>
2018-11-26 14:16:08 +0000

    Update to Login

---
+++ Bamp-DMD-F12/AppManager.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Firebase
+
+class AppManager {
+    
+    static let shared = AppManager()
+    
+    let storyboard = UIStoryboard (name: "Main", bundle: nil)
+    
+    var appContainer = AppContainerViewController!
+    
+    
+    private init () {}
+    
+    
+}
