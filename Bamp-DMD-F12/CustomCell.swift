0c6527437fa783aa41f05adc239f6edd769ebe5f
Laura McDonald (s5102942) <s5102942@mac38577.student.bournemouth.ac.uk>
2018-12-04 10:45:52 +0000

    Change

--- Bamp-DMD-F12/CustomCell.swift
+++ Bamp-DMD-F12/CustomCell.swift
@@ -2,13 +2,13 @@ import UIKit
 
 class CustomCell: UITableViewCell {
    
-    
+
     @IBOutlet weak var myLabel: UILabel!
-    
-    
+
+
     cell.myLabel.text = "Row \(indexPath.row)"
     
-    
+
     override func prepareForReuse() {
         super.prepadeForReuse()
    }
