83fa8bbc0cc43f2910f87cc0c2cfc200db1a6e41
Laura McDonald (s5102942) <s5102942@mac38578.student.bournemouth.ac.uk>
2018-12-11 10:52:23 +0000

    Login/Sign Up

---
+++ Bamp-DMD-F12/SignUpViewController.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Firebase
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailSignUp: UITextField!
+    @IBOutlet weak var passwordSignUp: UITextField!
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        guard let email = emailSignUp.text, let password = passwordSignUp.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            guard (result?.user) != nil else { return }
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "SelectionViewController") as! SelectionViewController
+            self.present(vc, animated: true, completion: nil)
+            
+        }
+        
+        
+        
+        
+    }
+}
