cd5a35d282ec67af53f4fb52649e25cf4cfc768d
Ewan McConnachie (s5115780) <s5115780@mac42950.student.bournemouth.ac.uk>
2019-01-07 14:44:35 +0000

    added comments to sign up

--- Bamp-DMD-F12/SignUpViewController.swift
+++ Bamp-DMD-F12/SignUpViewController.swift
@@ -6,12 +6,12 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailSignUp: UITextField!
     @IBOutlet weak var passwordSignUp: UITextField!
     @IBAction func signUpButton(_ sender: Any) {
-        
+ //this code allows the user to sign up to the app
         guard let email = emailSignUp.text, let password = passwordSignUp.text else { return }
         
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             guard (result?.user) != nil else { return }
-            
+   //if there is an error in signing up, this code wil return the user to the login page to try again
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(withIdentifier: "MainViewController") as! SelectionViewController
             self.present(vc, animated: true, completion: nil)
