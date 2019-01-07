36eb5820636d3a719f76436ca5551e58a2f1a889
Laura McDonald (s5102942) <s5102942@mac42944.student.bournemouth.ac.uk>
2019-01-07 14:15:31 +0000

    changes

--- Bamp-DMD-F12/LoginViewController.swift
+++ Bamp-DMD-F12/LoginViewController.swift
@@ -4,15 +4,15 @@ import Firebase
 import FirebaseAuth
 
 class LoginViewController: UIViewController {
-    
+
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
+    //these are the text fields where the user enters their details (email and pw)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailTextField.text = "test@test.com"
-        passwordTextField.text = "password"
+ 
     }
     
     @IBAction func login(_ sender: Any) {
@@ -22,10 +22,13 @@ class LoginViewController: UIViewController {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if let _ = result?.user {
                 self.dismiss(animated: true, completion: nil)
-                
+//This bit of code means that if the user has entered the correct email and password then the login screen will` dismiss
             }
 
         }
     
+    }
+    @IBAction func logout(_ sender: Any) {
+        AppManager.shared.logout()
     }
 }
